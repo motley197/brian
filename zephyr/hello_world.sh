76a0d71d4e5d97e1cb5fa9e6a6f989299442dd99
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2023-07-06 09:37:58 +0100

    Zephyr is for another day

--- zephyr/hello_world.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ~/zephyrproject/zephyr
-# From the root of the zephyr repository
-west build -p always -b rpi_pico samples/basic/blinky
-west build -p always -b rpi_pico samples/basic/blinky -- -DOPENOCD=/usr/local/bin/openocd -DOPENOCD_DEFAULT_PATH=/usr/local/share/openocd/scripts -DRPI_PICO_DEBUG_ADAPTER=cmsis-dap
