a2195609e65880e3ab2e1378ac54ab79a7548244
Chris Tipney <chris@tipney.net>
2023-07-16 17:11:22 +0100

    Update main.py
    update

--- uPython/examples/ping/main.py
+++ uPython/examples/ping/main.py
@@ -27,6 +27,8 @@ def cb(events):
 
 sx = SX1262(spi_bus=1, clk=10, mosi=11, miso=12, cs=3, irq=20, rst=15, gpio=2)
 
+
+
 # LoRa
 sx.begin(freq=868, bw=500.0, sf=12, cr=8, syncWord=0x12,
          power=-5, currentLimit=60.0, preambleLength=8,
